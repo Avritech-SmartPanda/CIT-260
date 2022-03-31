9f55916281cf2ca0166909739ba38d2c74bc7beb
Daphne Avril <daphneavril@outlook.com>
2022-03-31 08:30:41 +0200

    Week 12

--- week12/W12/src/Week12/Circle.java
+++ week12/W12/src/Week12/Circle.java
@@ -1,2 +1,42 @@
-package Week12;public class Circle {
+package Week12;
+
+/**
+ * @author Daphne Avril Museruka
+ */
+public class Circle extends Shape {
+    private double radius;
+
+    /**
+     * Creates a Circle with the provided id and radius
+     * @param id the desired id of the Circle
+     * @param radius the desired radius of the Circle
+     */
+    public Circle(int id, double radius) {
+        super(id);
+        this.setRadius(radius);
+    }
+
+    /**
+     * Gets the radius of a Circle
+     * @return the radius of a Circle
+     */
+    public double getRadius() {
+        return radius;
+    }
+
+    /**
+     * Sets the radius of a Circle
+     * @param radius the desired radius of a Circle
+     */
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    /**
+     * Calculates the area of a Circle
+     * @return the area of the Circle
+     */
+    public double getArea(){
+        return Math.PI * (this.getRadius() * this.getRadius());
+    }
 }
--- week12/W12/src/Week12/Square.java
+++ week12/W12/src/Week12/Square.java
@@ -1,2 +1,42 @@
-package Week12;public class Square {
+package Week12;
+
+/**
+ * @author Daphne Avril Museruka
+ */
+public class Square extends Shape {
+    private double size;
+
+    /**
+     * Creates a Square with the provided id and size
+     * @param id the desired id of the square
+     * @param size the desired size of the square
+     */
+    public Square(int id, double size) {
+        super(id);
+        this.setSize(size);
+    }
+
+    /**
+     * Gets the size of the Square
+     * @return the size of the square
+     */
+    public double getSize() {
+        return size;
+    }
+
+    /**
+     * Sets the size of the Square
+     * @param size the desired size of the square
+     */
+    public void setSize(double size) {
+        this.size = size;
+    }
+
+    /**
+     * Calculates the area of the square
+     * @return the area of the square
+     */
+    public double getArea(){
+        return this.getSize() * this.getSize();
+    }
 }
