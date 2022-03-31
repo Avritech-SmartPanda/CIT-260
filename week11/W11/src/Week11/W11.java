eb0f1a88202901242441a382da85735703f78cc4
Daphne Avril <daphneavril@outlook.com>
2022-03-31 08:30:03 +0200

    Week 12

---
+++ week11/W11/src/Week11/W11.java
@@ -0,0 +1,8 @@
+package Week11;
+
+public class W11 {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ week12/W12/src/Week12/W12.java
@@ -0,0 +1,8 @@
+package Week12;
+
+public class W12 {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
